74bc53f898ca96f99ecf07560c75fad0d041132f
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-18 15:33:25 -0700

    First

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def main():
     # Do not delete the return statement
     ########################################
     """
+    celsius = float(input('Enter a temperature in Celsius: '))
+    fahrenheit = ((9/5)*(celsius))+32
+    
+    print(f'The convserion to fahrenheit is {fahrenheit:.2f} ')
+    
     return celsius, fahrenheit
 
 
